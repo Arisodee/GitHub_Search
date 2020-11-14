2fd97601ecc5e38d14594316c6658f99a22103ff
Arisodee <arisodee@gmail.com>
2020-11-15 00:00:49 +0300

    Add profile and repository classes and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RepositoryComponent } from './repository/repository.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    RepositoryComponent
   ],
   imports: [
     BrowserModule,
